e31d809b9690db250d88846a267a2a5bc81e54ae
Erik Peterson <Erik.Exists@gmail.com>
2019-08-21 14:31:57 -0700

    Made this package pip installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wavetorch',
+    version='0.1.x',
+    description="Simulate waves, that learn!",
+    url='',
+    author='Shanhui Fans Lab (Stanford) + Erik Peterson (CMU)',
+    author_email='erik.exists@gmail.com',
+    license='MIT',
+    packages=['wavetorch'],
+    scripts=[],
+    zip_safe=False)
